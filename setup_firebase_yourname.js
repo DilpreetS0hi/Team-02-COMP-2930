e8191b6800670a2dcb8086b9a67d57340f1a8607
DilpreetSohi <DilpreetSohi@.github.com>
2019-05-02 10:32:00 -0700

    Firebase setup for login page

---
+++ setup_firebase_yourname.js
@@ -0,0 +1,14 @@
+(function(){  
+
+    var config = {
+    apiKey: "AIzaSyC3rK5fG2PwqAtjTQ2FhOCyzb4dIcXN2_0",
+    authDomain: "project-2930.firebaseapp.com",
+    databaseURL: "https://project-2930.firebaseio.com",
+    projectId: "project-2930",
+    storageBucket: "project-2930.appspot.com",
+    messagingSenderId: "16612303844"
+        };
+        firebase.initializeApp(config);
+  app_firebase = firebase; 
+})();
+
