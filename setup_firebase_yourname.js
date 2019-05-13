32dc0ba5a943674f81c487cda26254e1eb1e8cce
DilpreetSohi <DilpreetSohi@.github.com>
2019-05-13 12:30:50 -0700

    Changed setup

--- setup_firebase_yourname.js
+++ setup_firebase_yourname.js
@@ -1,14 +1,16 @@
 (function(){  
 
-    var config = {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyC3rK5fG2PwqAtjTQ2FhOCyzb4dIcXN2_0",
     authDomain: "project-2930.firebaseapp.com",
     databaseURL: "https://project-2930.firebaseio.com",
     projectId: "project-2930",
     storageBucket: "project-2930.appspot.com",
-    messagingSenderId: "16612303844"
-        };
-        firebase.initializeApp(config);
-  app_firebase = firebase; 
+    messagingSenderId: "16612303844",
+    appId: "1:16612303844:web:9d21a99f12b6b225"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 })();
 
