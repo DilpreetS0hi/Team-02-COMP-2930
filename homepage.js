96f4aff68f0023133c3c1cfaa4d4b360fa3b41d9
Alex Bratlavskyy <abratslavskyy@my.bcit.ca>
2019-05-17 14:19:28 -0700

    really fixed the glitch

--- homepage.js
+++ homepage.js
@@ -1,10 +1,10 @@
 function dis() {
-    if(document.getElementById("i-need-you").style.left == "-10000px"){
+    if(document.getElementById("i-need-you").style.left == "0px"){
         document.getElementById("i-need-you").style.left = "-10000px";
         document.getElementById("i-need-you").style.top = "-10000px";
     }else{
-        document.getElementById("i-need-you").style.left = 0;
-        document.getElementById("i-need-you").style.top = 0;
+        document.getElementById("i-need-you").style.left = "0px";
+        document.getElementById("i-need-you").style.top = "0px";
         
     }
   }
