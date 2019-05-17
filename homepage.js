ea449a1719510c96cd6bb42cf4c2fee8fdc598fb
Alex Bratlavskyy <abratslavskyy@my.bcit.ca>
2019-05-17 14:16:24 -0700

    fixed the glitch

---
+++ homepage.js
@@ -0,0 +1,10 @@
+function dis() {
+    if(document.getElementById("i-need-you").style.left == "-10000px"){
+        document.getElementById("i-need-you").style.left = "-10000px";
+        document.getElementById("i-need-you").style.top = "-10000px";
+    }else{
+        document.getElementById("i-need-you").style.left = 0;
+        document.getElementById("i-need-you").style.top = 0;
+        
+    }
+  }
