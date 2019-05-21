e9ade5e43ce71fd47a2704830dce76e923bef585
DilpreetSohi <DilpreetSohi@.github.com>
2019-05-21 13:33:43 -0700

    Pages Linked

--- fruits.js
+++ fruits.js
@@ -22,20 +22,60 @@ function dis() {
 
 var count = 0;
 document.getElementById("c1").addEventListener("click", function() {
-    ShowlistFruits("Apple");
+    ShowlistFruits("Apple/Info");
+    ShowlistFruits("Apple/Ingredients");
+    ShowlistFruits("Apple/Ingredients1");
+    ShowlistFruits("Apple/Method");
+    ShowlistFruits("Apple/Method1");
+    ShowlistFruits("Apple/Method2");
+    ShowlistFruits("Apple/Method3");
+    ShowlistFruits("Apple/Method4");
+    ShowlistFruits("Apple/Method5");
 });
 
 document.getElementById("c2").addEventListener("click", function() {
-    ShowlistFruits("Banana");
+    ShowlistFruits("Banana/Info");
+    ShowlistFruits("Banana/Ingredients");
+    ShowlistFruits("Banana/Ingredients1");
+    ShowlistFruits("Banana/Method");
+    ShowlistFruits("Banana/Method1");
+    ShowlistFruits("Banana/Method2");
+    ShowlistFruits("Banana/Method3");
+    ShowlistFruits("Banana/Method4");
+    ShowlistFruits("Banana/Method5");
 });
 document.getElementById("c3").addEventListener("click", function() {
-    ShowlistFruits("Berries");
+    ShowlistFruits("Berries/Info");
+    ShowlistFruits("Berries/Ingredients");
+    ShowlistFruits("Berries/Ingredients1");
+    ShowlistFruits("Berries/Method");
+    ShowlistFruits("Berries/Method1");
+    ShowlistFruits("Berries/Method2");
+    ShowlistFruits("Berries/Method3");
+    ShowlistFruits("Berries/Method4");
+    ShowlistFruits("Berries/Method5");
 });
 document.getElementById("c4").addEventListener("click", function() {
-    ShowlistFruits("Grapes");
+    ShowlistFruits("Grapes/Info");
+    ShowlistFruits("Grapes/Ingredients");
+    ShowlistFruits("Grapes/Ingredients1");
+    ShowlistFruits("Grapes/Method");
+    ShowlistFruits("Grapes/Method1");
+    ShowlistFruits("Grapes/Method2");
+    ShowlistFruits("Grapes/Method3");
+    ShowlistFruits("Grapes/Method4");
+    ShowlistFruits("Grapes/Method5");
 });
 document.getElementById("c5").addEventListener("click", function() {
-    ShowlistFruits("Citrus");
+    ShowlistFruits("Citrus/Info");
+    ShowlistFruits("Citrus/Ingredients");
+    ShowlistFruits("Citrus/Ingredients1");
+    ShowlistFruits("Citrus/Method");
+    ShowlistFruits("Citrus/Method1");
+    ShowlistFruits("Citrus/Method2");
+    ShowlistFruits("Citrus/Method3");
+    ShowlistFruits("Citrus/Method4");
+    ShowlistFruits("Citrus/Method5");
 });
 
 
@@ -45,9 +85,11 @@ function ShowlistFruits(catergory) {
     var promise = dbRef.once("value", function(snap) {
         list = snap.val();
         count++;
-        if(count>1){
+        if(count>9){
             var elem = document.getElementById('listofplant');
             elem.parentNode.removeChild(elem);
+            var brek = document.getElementById('break');
+            brek.parentNode.removeChild(brek);
         }
     });
 
@@ -58,11 +100,13 @@ function ShowlistFruits(catergory) {
 
 
 function DisplayList(list) {
-    console.log(list);
     var para = document.createElement("div");
-
-    console.log(count);
+    
+    var linebreak = document.createElement("br");
+    linebreak.setAttribute('id','break');
+    
     var bottonClick = document.createElement("div");
+    
     var inside = document.createElement("p");
 
     var doc = document.getElementById("content");
@@ -74,8 +118,11 @@ function DisplayList(list) {
     var node = document.createTextNode(list);
 
     inside.appendChild(node);
-    para.setAttribute("id", "listofplant");
+    
+    para.setAttribute('id', 'listofplant');
+    
     bottonClick.setAttribute("Class", "bottonClick");
 
+    doc.appendChild(linebreak);
 }
 
