40704fa069ee70b6ecaad6d649a6aef16170b3e4
Alex Bratlavskyy <abratslavskyy@my.bcit.ca>
2019-05-21 11:27:32 -0700

    halfway to perfection

--- expiry.js
+++ expiry.js
@@ -1,6 +1,4 @@
 // ----- constants -----
-// db
-let firebaseRef = firebase.database().ref(); 
 // elements
 let img = document.getElementById("img"),
     itemName = document.getElementById("itemName"),
@@ -18,6 +16,7 @@ let items = [],
 // ----- database -----
 // delete
 function deleteDB(node){
+    let firebaseRef = firebase.database().ref(); 
     firebase.auth().onAuthStateChanged(function(user){
         if (user){
             firebaseRef.child(node +"/"+ user.email.split(".")[0]).remove();
@@ -29,7 +28,7 @@ function deleteDB(node){
 function updateRec(){
     deleteDB("rec");
     for (let i = 0; i < recms.length; i++)
-        insertDBrec(recms[i], recms[i].time);
+        insertDBrec(recms[i]);
 }
 
 function updateItem(){
@@ -39,11 +38,12 @@ function updateItem(){
 }
 
 // push
-function insertDBrec(rec, t){
+function insertDBrec(rec){
+    let firebaseRef = firebase.database().ref(); 
     firebase.auth().onAuthStateChanged(function(user){
         if (user){
             firebaseRef.child("rec/"+ user.email.split(".")[0]).push().set({
-                time: t,
+                time: rec.time,
                 name:  rec.name,
                 img: rec.img,
                 days: rec.days,
@@ -54,7 +54,8 @@ function insertDBrec(rec, t){
     });
 }
 
-function insertDBitem(item){ 
+function insertDBitem(item){
+    let firebaseRef = firebase.database().ref(); 
     firebase.auth().onAuthStateChanged(function(user){
         if (user){
             firebaseRef.child("item/"+ user.email.split(".")[0]).push().set({
@@ -79,17 +80,17 @@ firebase.auth().onAuthStateChanged(function(user){
             rec.days = snapshot.val().days;
             rec.info = snapshot.val().info;
             rec.hit = snapshot.val().hit;
+            rec.time = snapshot.val().time;
             recms.push(rec);
-            if(snapshot.val().time != -1){
-                rec.time = snapshot.val().time;
+            if(rec.time != -1)
                 recns.push(rec);
-            }
         });
         ref2.on("child_added", function(snapshot){
             let item = new Object();
             item.name = snapshot.val().name;
             item.img = snapshot.val().img;
             item.date = snapshot.val().date;
+            item.noty = snapshot.val().noty;
             items.push(item);
         });
     }else
@@ -101,7 +102,8 @@ async function startupGeneration(){
     sorter = sortSelection(0);
     sortm = sortmSelection();
     // DO NO CHANGE END - TOP
-
+    // deleteDB("rec"); ////////////////////////////////
+    // deleteDB("item"); ///////////////////////////////
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
@@ -118,111 +120,110 @@ async function startupGeneration(){
             img : "apple.gif",
             days : [14],
             info : "fruits(1).html",
-            hit : 0
+            hit : 0,
+            time : -1
         };
-        insertDBrec(apple, -1);
-        recms.push(apple);
+        insertDBrec(apple);
             
-        // banana
-        let banana  = {
-            name : "Banana",
-            img : "banana1.gif",
-            days : [2],
-            info : "fruits(1).html",
-            hit : 0
-        };
-        insertDBrec(banana, -1);
-        recms.push(banana);
+        // // banana
+        // let banana  = {
+        //     name : "Banana",
+        //     img : "banana1.gif",
+        //     days : [2],
+        //     info : "fruits(1).html",
+        //     hit : 0,
+        //     time : -1
+        // };
+        // insertDBrec(banana);
             
-        // berries
-        let berries = {
-            name : "Berries",
-            img : "berries1.gif",
-            days : [2],
-            info : "fruits(1).html",
-            hit : 0
-        };
-        insertDBrec(berries, -1);
-        recms.push(berries);
+        // // berries
+        // let berries = {
+        //     name : "Berries",
+        //     img : "berries1.gif",
+        //     days : [2],
+        //     info : "fruits(1).html",
+        //     hit : 0,
+        //     time : -1
+        // };
+        // insertDBrec(berries);
             
-        // grapes
-        let grapes = {
-            name : "Grapes",
-            img : "SardonicExcellentIraniangroundjay-size_restricted.gif",
-            days : [3],
-            info : "fruits(1).html",
-            hit : 0
-        };
-        insertDBrec(grapes, -1);
-        recms.push(grapes);
+        // // grapes
+        // let grapes = {
+        //     name : "Grapes",
+        //     img : "SardonicExcellentIraniangroundjay-size_restricted.gif",
+        //     days : [3],
+        //     info : "fruits(1).html",
+        //     hit : 0,
+        //     time : -1
+        // };
+        // insertDBrec(grapes);
             
-        // citrus
-        let citrus = {
-            name : "Citrus",
-            img : "citrus.gif",
-            days : [2],
-            info : "fruits(1).html",
-            hit : 0
-        };
-        insertDBrec(citrus, -1);
-        recms.push(citrus);
+        // // citrus
+        // let citrus = {
+        //     name : "Citrus",
+        //     img : "citrus.gif",
+        //     days : [2],
+        //     info : "fruits(1).html",
+        //     hit : 0,
+        //     time : -1
+        // };
+        // insertDBrec(citrus);
 
-        // carrot
-        let carrot = {
-            name : "Carrot",
-            img : "Carrot.gif",
-            days : [2],
-            info : "vegetables.html",
-            hit : 0
-        };
-        insertDBrec(carrot, -1);
-        recms.push(carrot);
+        // // carrot
+        // let carrot = {
+        //     name : "Carrot",
+        //     img : "Carrot.gif",
+        //     days : [2],
+        //     info : "vegetables.html",
+        //     hit : 0,
+        //     time : -1
+        // };
+        // insertDBrec(carrot);
 
-        // potato
-        let potato = {
-            name : "Potato",
-            img : "sweetpotato.gif",
-            days : [2],
-            info : "vegetables.html",
-            hit : 0
-        };
-        insertDBrec(potato, -1);
-        recms.push(potato);
+        // // potato
+        // let potato = {
+        //     name : "Potato",
+        //     img : "sweetpotato.gif",
+        //     days : [2],
+        //     info : "vegetables.html",
+        //     hit : 0,
+        //     time : 0
+        // };
+        // insertDBrec(potato);
 
-        // kale
-        let kale = {
-            name : "Kale",
-            img : "kale.gif",
-            days : [2],
-            info : "vegetables.html",
-            hit : 0
-        };
-        insertDBrec(kale, -1);
-        recms.push(kale);
+        // // kale
+        // let kale = {
+        //     name : "Kale",
+        //     img : "kale.gif",
+        //     days : [2],
+        //     info : "vegetables.html",
+        //     hit : 0,
+        //     time : -1
+        // };
+        // insertDBrec(kale);
 
-        // zucchini
-        let zucchini = {
-            name : "Zucchini",
-            img : "zucchini.gif",
-            days : [2],
-            info : "vegetables.html",
-            hit : 0
-        };
-        insertDBrec(zucchini, -1);
-        recms.push(zucchini);
+        // // zucchini
+        // let zucchini = {
+        //     name : "Zucchini",
+        //     img : "zucchini.gif",
+        //     days : [2],
+        //     info : "vegetables.html",
+        //     hit : 0,
+        //     time : -1
+        // };
+        // insertDBrec(zucchini);
 
-        // tomato
-        let tomato = {
-            name : "Tomato",
-            img : "Tomato.gif",
-            days : [2],
-            info : "vegetables.html",
-            hit : 0
-        };
-        insertDBrec(tomato, -1);
-        recms.push(tomato);
+        // // tomato
+        // let tomato = {
+        //     name : "Tomato",
+        //     img : "Tomato.gif",
+        //     days : [2],
+        //     info : "vegetables.html",
+        //     hit : 0,
+        //     time : -1
+        // };
+        // insertDBrec(tomato);
     }
-            
     // generate
     // DO NO CHANGE - BOTTOM
     putItems();
@@ -232,6 +233,81 @@ async function startupGeneration(){
 }
 
 // ----- general expiry -----
+function createTempStart(){
+    return [
+        { // apple
+            name : "Apple",
+            img : "apple.gif",
+            days : [14],
+            info : "fruits(1).html",
+            hit : 0,
+            time : -1
+        },{ // banana
+            name : "Banana",
+            img : "banana1.gif",
+            days : [2],
+            info : "fruits(1).html",
+            hit : 0,
+            time : -1
+        },{ // berries
+            name : "Berries",
+            img : "berries1.gif",
+            days : [2],
+            info : "fruits(1).html",
+            hit : 0,
+            time : -1
+        },{ // grapes
+            name : "Grapes",
+            img : "SardonicExcellentIraniangroundjay-size_restricted.gif",
+            days : [3],
+            info : "fruits(1).html",
+            hit : 0,
+            time : -1
+        },{ // citrus
+            name : "Citrus",
+            img : "citrus.gif",
+            days : [2],
+            info : "fruits(1).html",
+            hit : 0,
+            time : -1
+        },{ // carrot
+            name : "Carrot",
+            img : "Carrot.gif",
+            days : [2],
+            info : "vegetables.html",
+            hit : 0,
+            time : -1
+        },{ // potato
+            name : "Potato",
+            img : "sweetpotato.gif",
+            days : [2],
+            info : "vegetables.html",
+            hit : 0,
+            time : 0
+        },{ // kale
+            name : "Kale",
+            img : "kale.gif",
+            days : [2],
+            info : "vegetables.html",
+            hit : 0,
+            time : -1
+        },{ // zucchini
+            name : "Zucchini",
+            img : "zucchini.gif",
+            days : [2],
+            info : "vegetables.html",
+            hit : 0,
+            time : -1
+        },{ // tomato
+            name : "Tomato",
+            img : "Tomato.gif",
+            days : [2],
+            info : "vegetables.html",
+            hit : 0,
+            time : -1
+        }
+    ];
+}
 function sortSelection(num){
     switch (num) {
     case 0:
@@ -367,7 +443,7 @@ function putItems(){
 
     items.sort(sorter);
     expitems.innerHTML = "";
-
+    
     let h = 29;
     for(let i=0; i<items.length; i++){
         h+=62
@@ -405,10 +481,10 @@ function newItem(){
     item.img = img.src;
     item.name = itemName.value;
     item.date = expirationDate.value;
-    item.noty = notifyMe.value;
+    item.noty = Number(notifyMe.value);
     
-    insertDBitem(item)
-    items.push(item);
+    insertDBitem(item);
+    //items.push(item); /////////////////////////////////////
     putItems();
     evalRecn(item);
 
@@ -464,7 +540,7 @@ search.addEventListener("keyup", function () {
                 r = a.name.localeCompare(b.name);
             return r;
         }
-    putRecm()
+    putRecm();
 });
 
 // ----- shared recomended & recent expiry -----
@@ -510,7 +586,6 @@ function addRec(rec) {
 // ----- in recomended expiry -----
 function putRecm(){
     let con = document.getElementById("rec");
-
     recms.sort(sortm);
     con.innerHTML = "";
 
@@ -551,36 +626,42 @@ function evalRecn(rec){
     let trec = {
         name : rec.name,
         img : rec.img,
-        date : rec.date
+        date : rec.date,
+        days : []
     };
-
-    for(let i=0; i<recms.length; i++){
+    for(let i=0; i<recms.length; i++)
         if(trec.img.lastIndexOf(recms[i].img) != -1 && trec.name == recms[i].name){
-            trec.days = recms[i].days;
+            // days
             if (daysTillExpiry(trec)>=0){
                 recms[i].days.push(daysTillExpiry(trec))
             }
+            trec.days = recms[i].days;
+            // hit
             recms[i].hit++;
-            recms[i].time = (new Date()).getTime();
+            // info
             trec.info = recms[i].info;
+            // time
+            recms[i].time = (new Date()).getTime();
         }
-    }
     for(let i = 0; i<recns.length; i++){
         if(trec.img.lastIndexOf(recns[i].img) != -1 && trec.name == recns[i].name){
             trec.days = recns[i].days;
             recns.splice(recns[i].unique,1);
         }
     }
-    if(typeof trec.days === "undefined"){
+    if(trec.days.length == 0){
+        // days
         if(daysTillExpiry(trec)>=0)
             trec.days = [daysTillExpiry(trec)];
-        else{
+        else
             trec.days = [0];
-        }
+        // hit
         trec.hit = 1;
+        // info
         trec.info = "";
+        // time
         trec.time = (new Date()).getTime();
-        recms.push(trec);
+        //recms.push(trec); //////////////////////////////
     }
 
     updateRec();
