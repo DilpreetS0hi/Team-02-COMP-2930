0e3ae3541ff6a2cd093ee07bff2d4fcec9314269
TanushParkashVerma <tanush.parkash16@gmail.com>
2019-05-20 21:36:25 -0700

    indentation done

--- homepage.js
+++ homepage.js
@@ -1,25 +1,44 @@
 function dis() {
-    if(document.getElementById("i-need-you").style.left == "0px"){
+    if (document.getElementById("i-need-you").style.left == "0px") {
         document.getElementById("i-need-you").style.left = "-10000px";
         document.getElementById("i-need-you").style.top = "-10000px";
-    }else{
+    } else {
         document.getElementById("i-need-you").style.left = "0px";
         document.getElementById("i-need-you").style.top = "0px";
-        
+
     }
-  }
+}
 
 
 
- 
+ <!--Easter egg -->
 var firstTime = true;
 var message = "try again!";
 var later = "easter egg surprise"
-function MouseoverMessageInAlertBox()
-{
-  if(firstTime) { alert(message); }
-  else { alert(later); }
-  firstTime = false;
+
+function MouseoverMessageInAlertBox() {
+    if (firstTime) {
+        alert(message);
+    } else {
+        alert(later);
+    }
+    firstTime = false;
 }
 
-   
+
+ <!--Slide show -->
+var slideIndex = 0;
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
